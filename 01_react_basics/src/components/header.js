16c8e9319a2b2ae35e3aa65110b71dc91e2a0f43
Alan Anaya Araujo <anaya.araujo@icloud.com>
2020-05-29 21:45:43 -0500

    Update header.js
    Added dynamic data

--- 01_react_basics/src/components/header.js
+++ 01_react_basics/src/components/header.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+const user = {
+    name: 'Alan',
+    lastName: 'Anaya',
+    age: '25'
+}
+
 const Header = () => {
-    return <div> This is just a normal header </div>
+    return (
+    <div>
+        <div>{user.name}</div>
+        <div>{user.lastName}</div>
+        <div>{user.age}</div>
+    </div>)
 }
 
 export default Header;
