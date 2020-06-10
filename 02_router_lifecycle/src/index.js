a0a2a5844e215af282e73fb58bb230cc89284409
Alan Anaya Araujo <anaya.araujo@icloud.com>
2020-06-10 15:46:07 -0500

    updates on router

--- 02_router_lifecycle/src/index.js
+++ 02_router_lifecycle/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+
+// CSS
+import './css/styles.css'
+
+// COMPONENTS
+import Home from './components/home'
+import Posts from './components/posts'
+import Profile from './components/profile'
+import PostItem from './components/post_item'
+
+//localhost/posts
+//localhost/profile/posts
+
+class App extends Component {
+  render(){ 
+    return (
+      <Router>
+        <div>
+          <header>
+            <NavLink to="/">Home</NavLink><br/>
+            <NavLink 
+              to="/posts"
+              activeClassName="selected"
+            >Posts</NavLink><br/>
+            <NavLink to={{
+              pathname: '/profile',
+              hash: '#alan',
+              search: '?profile=true'
+            }}>Profile</NavLink><br/>
+            <hr/>
+          </header>
+          <Switch>
+            <Route path="/posts/:id/:username" component={PostItem}/>
+            <Route path="/profile" component={Profile}/>
+            <Route path="/posts" component={Posts}/>
+            <Route path="/" exact component={Home}/>
+            <Route render={() => <h3>Oops!! 404</h3>}/>
+          </Switch>  
+          
+        </div>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App/>, 
+  document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
